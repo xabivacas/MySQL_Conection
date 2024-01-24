ab71325d86b9da04942802485ebbd5f6d6093c5f
Xabier Vacas <ikcty@plaiaundi.net>
2024-01-24 14:08:44 +0100

    Terminar update

--- src/MySQL_Contection/read/update.java
+++ src/MySQL_Contection/read/update.java
@@ -16,9 +16,10 @@ public class update {
 	private static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		
+		String id="";
 		String titulo="";
 		String descripcion="";
+		int count=0;
 		
 		try {
 			//Cargar libreria
@@ -29,16 +30,19 @@ public class update {
 			//Crear Statement
 				Statement st = conexion.createStatement();
 			
-			//Pedir datos y ejecutar query
-				System.out.println("Inserte titulo");
+			//Pedir el id de la tupla a cambiar
+				System.out.println("Inserte el id de la tarea que quiere cambiar");
+				id=scan.nextLine();
+				
+				System.out.println("Inserte el titulo nuevo");
 				titulo=scan.nextLine();
 				
-				System.out.println("Inserte la descripcion");
+				System.out.println("Inserte la descripcion nueva");
 				descripcion=scan.nextLine();
 				
-				String sql="INSERT INTO tareas(titulo, descripcion) VALUES ('"+titulo+"','"+descripcion+"')";
-				st.execute(sql);
-				System.out.println("Insert completado");
+				String sql="UPDATE tareas SET titulo='"+titulo+"',descripcion='"+descripcion+"' WHERE id="+id;
+				count = st.executeUpdate(sql);
+				System.out.println("Se han modificado: "+count);
 		} catch (ClassNotFoundException e) {
 			System.out.println("Error al importar liberira");
 			e.printStackTrace();
