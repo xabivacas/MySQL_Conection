cca638fbbe6788fa0baffb607bf0a05499ae89ee
Xabier Vacas <ikcty@plaiaundi.net>
2024-01-25 10:58:17 +0100

    PreparedStatement sin terminar

--- src/MySQL_Contection/read/update.java
+++ src/MySQL_Contection/read/update.java
@@ -2,6 +2,7 @@ package MySQL_Contection.read;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -27,14 +28,15 @@ public class update {
 			//Crear conexion
 				Connection conexion = DriverManager.getConnection("jdbc:mysql://"+HOST+"/"+BBDD,USER,PASSWORD);
 			
-			//Crear Statement
-				Statement st = conexion.createStatement();
-			
 			//Pedir el id de la tupla a cambiar
 				System.out.println("Inserte el id de la tarea que quiere cambiar");
 				id=scan.nextLine();
 				
-				ResultSet rs = st.executeQuery("select * from tareas where id="+id);
+				String sqlRead = "select * from tareas where id= ?";
+				PreparedStatement pst= conexion.prepareStatement(sqlRead);
+				pst.setString(1,id);
+				
+				ResultSet rs= pst.executeQuery();
 				
 				if(rs.next()) {
 					Tarea t = new Tarea();
@@ -53,8 +55,8 @@ public class update {
 				System.out.println("Inserte la descripcion nueva");
 				descripcion=scan.nextLine();
 				
-				String sql="UPDATE tareas SET titulo='"+titulo+"',descripcion='"+descripcion+"' WHERE id="+id;
-				cont = st.executeUpdate(sql);
+				String sqlUpdate="UPDATE tareas SET titulo= ? ,descripcion= ? WHERE id= ?";
+				pst.setString(1, titulo);
 				System.out.println("Se han modificado: "+cont);
 				
 		} catch (ClassNotFoundException e) {
